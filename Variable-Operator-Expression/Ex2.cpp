269ed733761fee6a374145485fdb9e52b5c59db3
tiffylee <46877769+tiffylee@users.noreply.github.com>
2020-04-16 10:22:00 +1000

    change name

---
+++ Variable-Operator-Expression/Ex2.cpp
@@ -0,0 +1,14 @@
+/*Write a program to display the following output using a single cout statement.
+
+   Subject            Marks
+   Mathematics     90 
+   Computer         77 
+   Chemistry        69 
+   */
+
+  #include <iostream>
+  int main(){
+    cout<<"\"subject\" " <<"\tmarks"<<"\nmathematic\t"
+		<<90<<"\ncomputer\t"<<77<<"\nchemistry\t"<<69;
+	return 0;
+  }
