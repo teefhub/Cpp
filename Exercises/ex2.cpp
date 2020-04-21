7d555fcf832a12ab14f3ce0dace55a914dd102ab
tiffylee <46877769+tiffylee@users.noreply.github.com>
2020-04-21 12:14:15 +1000

    delete

---
+++ Exercises/ex2.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author: Tiffany Lee 
+ * @Date: 2020-04-21 12:03:46 
+ * @Last Modified by: Tiffany Lee
+ * @Last Modified time: 2020-04-21 12:12:46
+ */
+
+
+/* convert a name into initials. */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string name;
+    getline(cin,name);
+    
+    return 0;
+}
